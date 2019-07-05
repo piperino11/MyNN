f13f734c50ab920ff731a7ca84e270c97f92b87a
piperino11 <37184273+piperino11@users.noreply.github.com>
2019-07-05 16:20:31 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,8 +3,8 @@ import numpy as np
 import h5py
 import json
 import torch
-from imageio import imresize, imread
-from imageio import imread, imresize
+from pillow import imresize, imread
+
 from tqdm import tqdm
 from collections import Counter
 from random import seed, choice, sample
